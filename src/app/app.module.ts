69475709fd3e204b476505cc41cde3b52c38fea6
thiagoprazeres <thiagoprazeres@gmail.com>
2018-04-11 18:08:05 -0300

    balada page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
-import { AboutPage } from '../pages/about/about';
+import { BaladaPage } from '../pages/balada/balada';
 import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
@@ -27,7 +27,7 @@ export const firebaseConfig = {
 @NgModule({
   declarations: [
     MyApp,
-    AboutPage,
+    BaladaPage,
     ContactPage,
     HomePage,
     TabsPage
@@ -42,7 +42,7 @@ export const firebaseConfig = {
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    AboutPage,
+    BaladaPage,
     ContactPage,
     HomePage,
     TabsPage
