3c4e6bac9ac194c2bbc079fb4c69bcd2d2254433
thiagoprazeres <thiagoprazeres@gmail.com>
2018-04-11 17:02:02 -0300

    SplashScreen

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,7 @@ import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
 
-import { StatusBar } from '@ionic-native/status-bar';
+// import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 @NgModule({
@@ -32,7 +32,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     TabsPage
   ],
   providers: [
-    StatusBar,
+    /*StatusBar,*/
     SplashScreen,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
