f716e0475af7c583ca57803786a5cabc5dd36bc4
thiagoprazeres <thiagoprazeres@gmail.com>
2018-04-11 18:13:52 -0300

    init page balada

--- src/pages/balada/balada.ts
+++ src/pages/balada/balada.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 @Component({
-  selector: 'page-about',
-  templateUrl: 'about.html'
+  selector: 'page-balada',
+  templateUrl: 'balada.html'
 })
 export class BaladaPage {
 
