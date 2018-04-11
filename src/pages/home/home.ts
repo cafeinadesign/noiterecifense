5d846b5880410ea378bf61da822f92ef4024716e
thiagoprazeres <thiagoprazeres@gmail.com>
2018-04-11 17:22:02 -0300

    Login to Facebook to authenticate

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-
+import { AngularFireAuth } from 'angularfire2/auth';
 import { AngularFireDatabase } from 'angularfire2/database';
+import * as firebase from 'firebase/app';
 import { Observable } from 'rxjs/Observable';
 
 
@@ -11,9 +12,29 @@ import { Observable } from 'rxjs/Observable';
 })
 export class HomePage {
   items: Observable<any[]>;
+  displayName;
 
-  constructor(public navCtrl: NavController, afDB: AngularFireDatabase) {
+  constructor(public navCtrl: NavController, 
+  	private afAuth: AngularFireAuth, 
+  	afDB: AngularFireDatabase) {
+    afAuth.authState.subscribe(user => {
+      if (!user) {
+        this.displayName = null;        
+        return;
+      }
+      this.displayName = user.displayName;      
+    });
     this.items = afDB.list('paginas').valueChanges();
+  }
+
+  signInWithFacebook() {
+    this.afAuth.auth
+      .signInWithPopup(new firebase.auth.FacebookAuthProvider())
+      .then(res => console.log(res));
+  }
+
+  signOut() {
+    this.afAuth.auth.signOut();
   }
 
 }
